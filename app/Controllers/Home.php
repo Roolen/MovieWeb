c635153e0f9e0bc5f166eb6dc5f3dea834cf28f1
Zhimovskij <zhimovskij@yandex.ru>
2020-02-08 13:18:11 +0500

    make registration method (#4)

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -11,7 +11,8 @@ class Home extends BaseController
 		return view('hello', $data);
 	}
 
-	public function showme($page = 'hello') {
+	public function showme($page = 'hello') 
+	{
 		if (! is_file(APPPATH.'/Views/'.$page.'.php'))
 		{
 			throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
@@ -22,6 +23,45 @@ class Home extends BaseController
 		echo view('Share/header', $data);
 		echo view($page, $data);
 		echo view('Share/footer', $data);
+	}
+
+	public function registration() 
+	{
+		$data_user = [
+			'first_name' => ($this->request->getPost('first_name') ?? ''),
+			'nickname' => ($this->request->getPost('nickname') ?? ''),
+			'email' => ($this->request->getPost('email') ?? ''),
+			'password' => ($this->request->getPost('password') ?? ''),
+			'phone_number' => ($this->request->getPost('phone_number') ?? '')
+		];
+
+		$model = new UsersModel();
+		$check = $model->checkUser($data_user['nickname'],
+								   $data_user['email'],
+								   $data_user['phone_number']);
+
+		foreach ($check as $key=>$value) {
+			if ($value) {
+				$check['success'] = false;
+				$this->response->setStatusCode(409)
+				               ->setJSON(false);
+				echo json_encode($check);
+				return;
+			}
+		}
+
+		if ($model->createUser($data_user)) {
+			$check['success'] = true;
+			$this->response->setStatusCode(201)
+			               ->setJSON(false);
+			echo json_encode($check);
+		}
+		else {
+			$check['success'] = false;
+			$this->response->setStatusCode(400)
+			               ->setJSON(false);
+			echo json_encode($check);
+		}
 	}
 
 }
--- app/Models/UsersModel.php
+++ app/Models/UsersModel.php
@@ -5,6 +5,14 @@ use CodeIgniter\Model;
 class UsersModel extends Model
 {
     protected $table = 'users';
+    protected $primaryKey = 'id';
+    protected $allowedFields = [
+        'first_name',
+        'nickname',
+        'email',
+        'password',
+        'phone_number'
+    ];
 
     public function getUsers($nick = false)
     {
@@ -16,5 +24,45 @@ class UsersModel extends Model
         return $this->asArray()
                     ->where(['nickname' => $nick])
                     ->first();
+    }
+
+    public function createUser(array $data_user)
+    {
+        if (
+            $data_user['first_name'] === '' ||
+            $data_user['nickname'] === '' ||
+            $data_user['email'] === '' ||
+            $data_user['password'] === '' ||
+            $data_user['phone_number'] === ''
+           ) 
+        {
+                return false;
+        }
+
+        $password = $data_user['password'];
+        $data_user['password'] = password_hash($password, PASSWORD_ARGON2ID);
+
+        $this->insert($data_user);
+        return true;
+    }
+
+    public function checkUser(string $nickname, string $email, string $phone)
+    {
+        $user = $this->asArray()
+                     ->where(['nickname' => $nickname])
+                     ->orWhere(['email' => $email])
+                     ->orWhere(['phone_number' => $phone])
+                     ->first();
+
+        if ($user == null) {
+            return ['all' => false];
+        }
+        else {
+            return [
+                'nickname' => ($nickname == $user['nickname']),
+                'email' => ($email == $user['email']),
+                'phone' => ($phone == $user['phone_number'])
+            ];
+        }
     }
 }
