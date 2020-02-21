57860f09d419b2e6cd8e7cfd74913fc293c5166d
Zhimovskij <zhimovskij@yandex.ru>
2020-02-21 14:26:56 +0500

    make mail page

--- app/Controllers/Mail.php
+++ app/Controllers/Mail.php
@@ -1,15 +1,131 @@
 <?php namespace App\Controllers;
 
 use App\Models\UsersModel;
+use App\Models\MessagesModel;
 
 class Mail extends BaseController
 {
     public function index()
     {
-        $data = ['title' => 'Mail'];
+        $session = session();
+
+        if (! $session->has('isAuth')) {
+            $this->response->redirect(base_url());
+        }
+
+        $data = [
+            'title' => 'Mail',
+        ];
 
         echo view('Share/header', $data);
         echo view('mail');
         echo view('Share/footer');
+    }
+
+    public function senders()
+    {
+        $session = session();
+        $request = $this->request->getJSON(true);
+
+        $this->response->setJSON(false);
+        if (! $session->has('isAuth')) {
+            return json_encode(['isAuth' => false]);
+        }
+
+        $idUser = (int)$session->get('idUser');
+        $messagesModel = new MessagesModel();
+        $senders = $messagesModel->getSenders($idUser);
+
+        if (! $senders) {
+            return json_encode(['isEmpty' => true]);
+        }
+
+        $usersModel = new UsersModel();
+        $users = $usersModel->getUserSById($senders);
+
+        $usersData = [];
+        foreach ($users as $user) {
+            $usersData[] = [
+                'nickname' => $user['nickname'],
+                'avatar' => ($user['path_avatar'])
+                            ? $user['path_avatar']
+                            : base_url().'/images/employee.svg'
+            ];
+        }
+
+        return json_encode($usersData);
+    }
+
+    public function messages()
+    {
+        $session = session();
+        $request = $this->request->getJSON(true);
+
+        $this->response->setJSON(false);
+        if (! $session->has('isAuth')) {
+            $this->response->redirect(base_url());
+        }
+
+        $idUser = (int)$session->get('idUser');
+        $senderNick = $request['userNick'];
+
+        $usersModel = new UsersModel();
+        $senderUser = $usersModel->getUser($senderNick);
+
+        if (! $senderUser) {
+            return json_encode(['nickIncorrect' => true]);
+        }
+
+        $idSender = $senderUser['id'];
+
+        $messagesModel = new messagesModel();
+        $messages = $messagesModel->getMessages($idUser, $idSender);
+
+        $messagesData = [];
+        foreach ($messages as $message) {
+            $messagesData[] = [
+                'isUser' => ($message['id_sender'] == $idUser),
+                'text' => $message['text_message'],
+                'date' => $message['date_send']
+            ];
+        }
+
+        return ($messages)
+               ? json_encode($messagesData)
+               : json_encode(['isEmpty' => true]);
+    }
+
+    public function sendMessage()
+    {
+        $session = session();
+        $request = $this->request->getJSON(true);
+        $this->response->setJSON(false);
+
+        if (! $session->has('isAuth')) {
+            return json_encode(['isAuth' => false]);
+        }
+
+        $text = $request['text'];
+        $nickRecip = $request['nick'];
+        $idUser = (int)$session->get('idUser');
+
+        $usersModel = new UsersModel();
+        $user = $usersModel->getUser($nickRecip);
+
+        if (! $user) {
+            return json_encode(['success' => false, 'nickIncorrect' => true]);
+        }
+
+        $idRecip = $user['id'];
+
+        $messagesModel = new MessagesModel();
+        $newMessage = $messagesModel->createMessage($idUser, $idRecip, $text);
+
+        if ($newMessage) {
+            return json_encode(['success' => true]);
+        }
+        else {
+            return json_encode(['success' => false]);
+        }
     }
 }
--- app/Models/MessagesModel.php
+++ app/Models/MessagesModel.php
@@ -8,7 +8,77 @@ class MessagesModel extends Model
     protected $primaryKey = "id";
     protected $allowedFields = [
         'id_sender',
-        'id_resipient',
+        'id_recipient',
         'text_message'
     ];
+
+    public function getSendMessages(int $idUser)
+    {
+        $messages = $this->asArray()
+                         ->where(['id_sender' => $idUser])
+                         ->findAll();
+
+        return ($messages)
+               ? $messages
+               : false;
+    }
+
+    public function getWritedUsers(int $idUser)
+    {
+        $idsUsers = $this->asArray()
+                         ->where(['id_recipient' => $idUser])
+                         ->findAll();
+
+        return ($idsUsers)
+               ? $idsUsers
+               : false;
+    }
+
+    public function getMessages(int $idUserOne, int $idUserTwo)
+    {
+        $messages = $this->asArray()
+                         ->whereIn('id_sender', [$idUserOne, $idUserTwo])
+                         ->orWhereIn('id_recipient', [$idUserOne, $idUserTwo])
+                         ->findAll();
+
+        return ($messages)
+               ? $messages
+               : false;
+    }
+
+    public function getSenders(int $idUser)
+    {
+        $messages = $this->asArray()
+                        ->where(['id_sender' => $idUser])
+                        ->orWhere(['id_recipient' => $idUser])
+                        ->findAll();
+
+        if ($messages) {
+            $idUsers = [];
+            foreach ($messages as $message) {
+                if ($message['id_sender'] != $idUser) {
+                    $idUsers[] = $message['id_sender'];
+                }
+                else if ($message['id_recipient'] != $idUser) {
+                    $idUsers[] = $message['id_recipient'];
+                }
+            }
+
+            return array_unique($idUsers);
+        }
+        else {
+            return false;
+        }
+    }
+
+    public function createMessage(int $idSender, int $idRecipient, string $text)
+    {
+        $message = $this->insert(['id_sender' => $idSender,
+                                  'id_recipient' => $idRecipient,
+                                  'text_message' => $text]);
+
+        return ($message)
+               ? $message
+               : false;
+    }
 }
