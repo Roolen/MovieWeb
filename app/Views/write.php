442ddb39d021550243422df45bb814974026e31e
Zhimovskij <zhimovskij@yandex.ru>
2020-02-25 19:28:36 +0500

    make change post page

--- app/Views/write.php
+++ app/Views/write.php
@@ -1,20 +1,35 @@
 <link rel="stylesheet" href="<?= base_url(); ?>/css/write.css"/>
 <div id="write-app">
     <main class="main">
-        <h2 class="title-label">Новая статья</h2>
-        <input v-model="title" type="text" class="default-field title-field" placeholder="Заголовок">
+        
+        <?php if ($isRewrite) : ?>
+            <h2 class="title-label">Change <?= $titlePost ?></h2>
+            <?php else : ?>
+                <h1 class="title-label">Новая статья</h1>
+            <input v-model="title" type="text" class="default-field title-field" placeholder="Заголовок">
+        <?php endif ?>
+
         <h2 class="text-label">Текст</h2>
-        <textarea v-model="text" class="text-field" placeholder="Напишите текст поста..." required></textarea>
+        <textarea v-model="text" class="text-field" placeholder="Напишите текст поста..." required ></textarea>
         <input v-model="tags" type="text" class="default-field tag-field" placeholder="Тэги">
         <div class="tag-message">Тэги пишуться через запятую</div>
         <label for="file-input" class="default-button add-image">Добавить изображение</label>
         <input @change="changeImage" class="file-input" id="file-input" type="file" accept=".jpg, .png, .jpeg" />
         <div :style="messageStyle" class="message-publish">{{ messagePublish }}</div>
-        <button @click="createPost()" class="default-button publish-button">Опубликовать</button>
+        <?php if ($isRewrite) : ?>
+            <button @click="changePost()" class="default-button publish-button">Имзенить</button>
+            <button @click="deletePost()" class="default-button delete-button">Удалить пост</button>
+        <?php else : ?>
+            <button @click="createPost()" class="default-button publish-button">Опубликовать</button>
+        <?php endif ?>
     </main>
 </div>
 <script>
     const baseUrl = "<?= base_url(); ?>"
+    "<?= $text; ?>"
+    const text = decodeURI("<?= (isset($text))?$text:''; ?>")
+    const tags = "<?= (isset($tags))?$tags:''; ?>"
+    const title = "<?= (isset($titlePost))?$titlePost:''; ?>"
 </script>
 <script src="<?= base_url(); ?>/js/vue.min.js"></script>
 <script src="<?= base_url(); ?>/js/write.js"></script>
